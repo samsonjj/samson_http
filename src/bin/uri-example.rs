d1d7aec6d26609dba4a808c976b4224ac842cd81
js <jonathansamsonvcu@gmail.com>
2022-10-08 02:24:21 +0000

    remove uriparse library

--- src/bin/uri-example.rs
+++
@@ -1,7 +0,0 @@
-use uriparse::URI;
-
-fn main() {
-    let uri = URI::try_from("example.com/my/path?my=query").unwrap();
-    println!("path : {}", uri.path());
-    println!("query: {:?}", uri.query());
-}
